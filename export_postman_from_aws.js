341ef65258dbb2ccaba578cdcbbfc4098f9c37e7
hparekh <hparekh@nd.edu>
2018-08-07 08:21:56 -0700

    Adds a script to export a postman config from APIGateway

---
+++ export_postman_from_aws.js
@@ -0,0 +1,35 @@
+'use strict';
+const args = require('yargs').argv;
+const url = require('url');
+const fs = require('fs');
+const util = require('util');
+const AWS = require('aws-sdk');
+
+var parsed_url = url.parse(args.INVOKE_URL);
+var restApiId = parsed_url.hostname.split(".")[0]
+var stageName = parsed_url.pathname.replace(/\//g, '')
+
+var params = {
+  exportType: 'swagger', /* required */
+  restApiId: restApiId, /* required */
+  stageName: stageName, /* required */
+  accepts: 'application/json',
+  parameters: {
+    'extensions': 'postman',
+  }
+};
+
+var apigateway = new AWS.APIGateway();
+
+apigateway.getExport(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else {
+    console.log("Successful response from APIGateway"); // successful response
+    fs.writeFile("outputs/postman.json", util.inspect(JSON.parse(data.body)), 'utf8', function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("The Postman file was saved!");
+    });
+  }
+});
